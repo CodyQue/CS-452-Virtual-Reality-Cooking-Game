4ef049323ee1de1f4acb05e15ed082bf3f2d83d8
Christopher (Cody) Querubin <cody.querubin130@gmail.com>
2023-11-13 14:58:46 -0500

    Added background

--- src/index.js
+++ src/index.js
@@ -5,11 +5,30 @@ import './index.css';
   class Game extends React.Component {
     render() {
       return (
-        <div className="title">
-          <h1>
-            Test Website For CS 452 Project
-          </h1>
+        <div className="container text-center" style={{ backgroundColor: '#BBDEFB' }}>
+        <div className="row">
+          <div className="col">
           </div>
+          <div className="col-10">
+            <h1>
+              CS 452: VIRTUAL REALITY- Cooking Game
+            </h1>
+          </div>
+          <div className="col">
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+          </div>
+          <div className="col-5">
+            <h4>
+              By Cody, Chase, Azka, Loc
+            </h4>
+          </div>
+          <div className="col">
+          </div>
+        </div>
+      </div>
       );
     }
   }
