bc13cffdb52a8e56b02731158c7998157c7a6a80
Christopher (Cody) Querubin <cody.querubin130@gmail.com>
2023-12-01 11:33:24 -0500

    Minor Fixes

--- src/Video.js
+++ src/Video.js
@@ -1,13 +1,13 @@
 function Video() {
     return (
       <div className="w-full h-screen overflow-hidden relative grid place-items-center">
-      <div id="background3"className="backgroundDiv">
+      <div id="background">
       <div className="container text-center" style={{marginTop: '1%'}}>
      <div className="row" style={{marginTop: '1%'}}>
           <div className="col">
           </div>
           <style></style>
-          <div className="col-10" style={{marginTop: '4%', width:"100%", height:"100%"}}>
+          <div className="col-10" style={{width:"100%", height:"100%"}}>
             <h1 className="responsive-heading" style = {{color: 'white'}}>
               <b>Welcome To The CS 452: VIRTUAL REALITY- Cooking Game Website!</b>
             </h1>
